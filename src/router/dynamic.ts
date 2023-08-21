2646a030a64426475ff9d4a447ea43d27477cef0
刘欢 <1641186065@qq.com>
2023-08-21 18:39:35 +0800

    build(router): 更换路径

--- src/router/dynamic.ts
+++ src/router/dynamic.ts
@@ -9,7 +9,7 @@ export const dynamicRoutes: RouteRecordRaw[] = [
       title: 'Welcome',
       menuId: 4123040
     },
-    component: () => import(/* webpackChunkName: "DashboardView" */ '../views/DashboardView.vue')
+    component: () => import(/* webpackChunkName: "DashboardView" */ '@/views/DashboardView.vue')
   },
   {
     path: 'HomeView',
@@ -19,18 +19,18 @@ export const dynamicRoutes: RouteRecordRaw[] = [
       menuId: 2713025,
       title: 'HomeView'
     },
-    component: () => import(/* webpackChunkName: "HomeView" */ '../views/HomeView.vue')
+    component: () => import(/* webpackChunkName: "HomeView" */ '@/views/HomeView.vue')
   },
 
   {
     path: 'nested1',
     name: 'nested1',
-    component: () => import(/* webpackChunkName: "nested1" */ '../views/nested/menu1.vue'),
+    component: () => import(/* webpackChunkName: "nested1" */ '@/views/nested/menu1.vue'),
     children: [
       {
         path: 'nested2',
         name: 'nested2',
-        component: () => import(/* webpackChunkName: "nested2" */ '../views/nested/menu2.vue'),
+        component: () => import(/* webpackChunkName: "nested2" */ '@/views/nested/menu2.vue'),
 
         children: [
           {
@@ -40,7 +40,7 @@ export const dynamicRoutes: RouteRecordRaw[] = [
               affix: true,
               title: 'nested'
             },
-            component: () => import(/* webpackChunkName: "nested3" */ '../views/nested/menu3.vue')
+            component: () => import(/* webpackChunkName: "nested3" */ '@/views/nested/menu3.vue')
           }
         ]
       }
@@ -52,6 +52,6 @@ export const dynamicRoutes: RouteRecordRaw[] = [
     meta: {
       menuId: 1110027
     },
-    component: () => import(/* webpackChunkName: "TableView" */ '../views/TableView.vue')
+    component: () => import(/* webpackChunkName: "TableView" */ '@/views/TableView.vue')
   }
 ]
